33dff3926583ece987f14a984eae23d32de75607
MaksymBuzin <maksymbuzin@gmail.com>
2022-07-02 10:04:42 -0400

    Initial commit

---
+++ src/test/java/myPractice/get_Posts.java
@@ -0,0 +1,36 @@
+package myPractice;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class get_Posts {
+
+    @Test
+    public void validatePrettyPrint(){
+        RestAssured.given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .prettyPrint();
+    }
+
+    @Test
+    public void verifyStatusCode(){
+        RestAssured.given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void verifyContentType(){
+        RestAssured.given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/posts")
+                .then()
+                .assertThat()
+                .contentType(ContentType.JSON);
+    }
+}
